d273f1e945c4eed482393a95bd956ed66688f261
rednes <528249+rednes@users.noreply.github.com>
2023-02-13 09:57:44 +0900

    npx cdk init --language=typescript

---
+++ test/cdk-cloudfront-signed-cookies-sample.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCloudfrontSignedCookiesSample from '../lib/cdk-cloudfront-signed-cookies-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cloudfront-signed-cookies-sample-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCloudfrontSignedCookiesSample.CdkCloudfrontSignedCookiesSampleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
